e09c5cc558eb32f5b59fe4ed46faad989a680197
Randall Leeds <randall@bleeds.info>
2014-03-06 15:17:03 -0800

    Add term parser

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 /** @jsx React.DOM */
 var FacetedSearch = React.createClass({
   render: function () {
-    return <input type="search"></input>
+    return <input ref="query" type="search"></input>
+  },
+
+  _parseTerms: function () {
+    var query = this.refs.query.getDOMNode().value;
+    var terms = query.match(/(\w+(?::|$)(?: ?\b\w+\b(?!:))*)+/g);
+
+    if (terms) return terms.map(function (t) {
+      return t.split(':').map(function (s) {
+        return s.trim();
+      });
+    });
   }
 });
